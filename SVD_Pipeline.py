0a04373f85d09fb01409e7def6d4c0fcc182dcde
Kreative-Karuna <137216901+Kreative-Karuna@users.noreply.github.com>
2023-06-22 00:41:49 +0530

    Add files via upload

--- SVD_Pipeline.py
+++ SVD_Pipeline.py
@@ -1,35 +1,4 @@
 '''Dimension Reduction - SVD 
-Business Problem: Processing huge files (e.g., images) for real time applications is not feasible. 
-
-# CRISP-ML(Q):
-    Business & Data Understanding:
-        Business Problem: Huge files to be analyzed requires a lot of compute and is time consuming
-        Business Objective: Minimize the compute & time for processing
-        Business Constraints: Minimize the low resolution images
-        
-        Success Criteria:
-            Business: Reduce the compute required by 50%
-            ML: Get at least 50% compression
-            Economic: ROI of at least $500K over a period of 1 year
-
-# Data Collection
-
-# Data: 
-#    The university details are obtained from the US Higher Education Body and is publicly available for students to access.
-# 
-# Data Dictionary:
-# - Dataset contains 25 university details
-# - 8 features are recorded for each university
-# 
-# Description:
-# - Univ - University Name
-# - State - Location (state) of the university
-# - SAT - Average SAT score for eligibility
-# - Top10 - % of students who ranked in top 10 in their previous academics
-# - Accept - % of students admitted to the universities
-# - SFRatio - Student to Faculty ratio
-# - Expenses - Overall cost in USD
-# - GradRate - % of students who graduate'''
 
 # Install the required packages if not available
 
